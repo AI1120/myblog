79f0c54385f7f6132579f7479194745faa45cf8a
fool <fool@gmail.com>
2023-06-29 23:39:47 -0400

    First commit

---
+++ student/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.student_view, name = 'student_show'),
+]
---
+++ student/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def student_view(request):
+    x = []
+    for i in range(10):
+        x.append(i)
+    return HttpResponse("<h1>DataFlair Django Tutorials</h1>The Digits are {0}".format(x))
